aedb070bd6af0d739c427885f3be5e2a031f6127
Nikita Kozlenko <ggkozlenko@gmail.com>
2024-08-21 15:06:34 +0200

    init

---
+++ src/app/components/Experience.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Canvas from '@react-three/fiber';
+import { Box } from '@react-three/drei';
+
+const Experience = () => {
+  return (
+    <>
+      <Canvas>
+        <Box></Box>
+      </Canvas>
+    </>
+  );
+};
+
+export default Experience;
